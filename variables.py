6798f2a224d1808e5a5302a7ddde86950ee23a01
Tribrit <trizerbritney@gmail.com>
2024-05-31 14:26:39 +0300

    variables2 added

---
+++ variables.py
@@ -0,0 +1,11 @@
+my_var=3
+#To display the variable
+print(my_var)
+
+#To change variable value assigned to 100
+my_var=100
+print(my_var)
+#increase my value by 4
+my_var=my_var+4
+
+print(my_var)
---
+++ variables2.py
@@ -0,0 +1,7 @@
+#create a variables
+num_days=366
+num_hours_per_day=24
+num_of_minutes_per_hour=60
+num_of_seconds_per_hour=60
+total_seconds =(num_hours_per_day*num_of_minutes_per_hour*num_of_seconds_per_hour)
+print(total_seconds )
