03213bcf6a557f9c6e30969ab481c4507333e0b6
Tribrit <trizerbritney@gmail.com>
2024-05-31 18:00:18 +0300

    lists.c

---
+++ lists.py
@@ -0,0 +1,10 @@
+names="jordan,emma,lavine,warui,nyaga ,caleb"
+print(names)
+names_list=["jordan","Emma","lavine","warui","nyaga","caleb"]
+print(names_list)
+#To find the no. of items in the list we use len()
+print(len(names_list))
+print("second  name:",names_list[2])
+print("final two names:",names_list[4:])
+names_list.remove ("warui")
+print(names_list)
