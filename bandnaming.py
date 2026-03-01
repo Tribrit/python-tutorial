f92f17bc9ce5b35c27af04a1fe8aeb3d2eab8e43
Tribrit <trizerbritney@gmail.com>
2026-03-01 15:35:42 +0300

    added

--- bandnaming.py
+++ bandnaming.py
@@ -1,3 +1,5 @@
+#this is a band naming program that showcases the use of string concatenation and user input
+
 print("welcome to band naming program!")
 place=input("Where do you come from?")
 fav=input("who is your favourite person?")
